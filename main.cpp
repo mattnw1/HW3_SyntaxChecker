4cff38db79bdf171afd2ac5201c500641a8d4c13
Matt Nwerem <MattNwerem@Matts-MacBook-Pro-5.local>
2019-10-10 09:50:49 -0700

    testing stack more

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include "GenStack.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  GenStack myStack(10);
+
+  myStack.push('R');
+  cout << "Push 1: " << myStack.peek() << endl;
+
+  myStack.push('a');
+  cout << "Push 2: " << myStack.peek() << endl;
+
+  myStack.push('d');
+  cout << "Push 3: " << myStack.peek() << endl;
+
+  myStack.push('a');
+  cout << "Push 4: " << myStack.peek() << endl;
+
+
+
+  cout << "Popping: " << myStack.pop() << endl;
+  cout << "Peeking: " << myStack.peek() << endl;
+
+  myStack.push('F');
+  cout << "Peeking: " << myStack.peek() << endl;
+
+  while(!myStack.isEmpty())
+  {
+    cout << "Popping: " << myStack.pop() << endl;
+  }
+
+
+  return 0;
+}
